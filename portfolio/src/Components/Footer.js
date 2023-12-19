07ef2575dc113e4b06fc27a49333814f27fb6311
cynthiachinenye <cynthiachiagu@gmail.com>
2023-12-19 15:45:14 +0100

    new

---
+++ portfolio/src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+    <hr/>
+    <div className='division'>
+    
+    </div>
+      
+    </div>
+  )
+}
+
+export default Footer
